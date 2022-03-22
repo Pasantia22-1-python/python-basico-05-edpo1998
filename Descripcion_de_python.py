d3b72e6fbef261065384c3411af71838458ccf91
edpo1998 <edpo11081998@gmail.com>
2022-03-22 14:02:59 -0600

    video

---
+++ Descripcion_de_python.py
@@ -0,0 +1,17 @@
+# Ciencias
+
+# CIENCIA
+# Existen diferentes librerias que podemos utilizar dentro del mundo de las ciencias y datos
+# CLOUD
+# Existen tambien en CLI que nos permiten controlar data centers
+    #aws
+    #gcloud
+    #rebound
+    #geeknote
+# APLICACIONES WEB
+    #Django
+    #Flask
+    #Bottle
+    #Chalice
+    #Webapp2
+    #Gunicorn
